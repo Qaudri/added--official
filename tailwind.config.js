65b10db169f1e4b8893d5070b71e0793283352e5
Qaudri <aquadirmuhammad@gmail.com>
2022-03-21 15:36:12 +0100

    init: started project development

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+
+      colors: {
+        
+        primary: {
+          '100': '#058182'
+        },
+
+        secondary: {
+          '100': '#f26522'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
