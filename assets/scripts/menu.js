61e8224f25590724ff94a95821a5d633b6b77b6c
Qaudri <aquadirmuhammad@gmail.com>
2022-03-22 15:29:51 +0100

    chore: javascript functionality improved

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,5 +1,5 @@
 let toggleMenu = document.getElementById("menu_button")
-let menuItems = document.getElementById("menu_items");
+let menuItems = document.getElementById("menu_items")
 let menuCTA = document.getElementById("menu_cta");
 let menuState = false
 
@@ -11,9 +11,9 @@ function toggleMenuState() {
 
   if (this.menuState = true) {
     menuItems.style.display = "block";
-    menuCTA.style.display = "block"
+    menuCTA.style.display = "block";
   } else {
     menuItems.style.display = "none";
-    menuCTA.style.display = "none"
+    menuCTA.style.display = "none";
   }
 }
