db8647b2442e74b4e637409365a712f4d604419f
Qaudri <aquadirmuhammad@gmail.com>
2022-03-22 14:02:54 +0100

    chore: javascript functionality added for menu

---
+++ assets/scripts/menu.js
@@ -0,0 +1,19 @@
+let toggleMenu = document.getElementById("menu_button")
+let menuItems = document.getElementById("menu_items");
+let menuCTA = document.getElementById("menu_cta");
+let menuState = false
+
+
+toggleMenu.addEventListener("click", toggleMenuState);
+
+function toggleMenuState() {
+  this.menuState =! this.menuState;
+
+  if (this.menuState = true) {
+    menuItems.style.display = "block";
+    console.log("true");
+  } else {
+    menuItems.style.display = "none";
+    console.log(false);
+  }
+}
