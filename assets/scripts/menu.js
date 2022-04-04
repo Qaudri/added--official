d84539ed2c5ea68c7b49d15ed355546d8494ade8
Qaudri <aquadirmuhammad@gmail.com>
2022-04-04 17:17:16 +0100

    chore: menu script responsive view

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,20 +1,23 @@
-let menuBtn = document.getElementById("menu_button")
-let closeBtn = document.getElementById("close_button")
-let menuItems = document.getElementById("menu_items")
+if ((window.innerWidth < 1024)) {
 
-menuBtn.addEventListener("click", openMenu);
-closeBtn.addEventListener("click", closeMenu);
-menuItems.addEventListener("click", closeMenu);
+  let menuBtn = document.getElementById("menu_button")
+  let closeBtn = document.getElementById("close_button")
+  let menuItems = document.getElementById("menu_items")
+
+  menuBtn.addEventListener("click", openMenu);
+  closeBtn.addEventListener("click", closeMenu);
+  menuItems.addEventListener("click", closeMenu);
 
 
-function openMenu(params) {
-  menuItems.style.display = "block";
-  menuBtn.style.display = "none";
-  closeBtn.style.display = "block";
-}
+  function openMenu(params) {
+    menuItems.style.display = "block";
+    menuBtn.style.display = "none";
+    closeBtn.style.display = "block";
+  }
 
-function closeMenu(params) {
-  menuItems.style.display = "none";
-  menuBtn.style.display = "block";
-  closeBtn.style.display = "none";
+  function closeMenu(params) {
+    menuItems.style.display = "none";
+    menuBtn.style.display = "block";
+    closeBtn.style.display = "none";
+  }
 }
