b935e13b432f2cba2ba56cc26fc9e3dee7569764
Qaudri <aquadirmuhammad@gmail.com>
2022-04-19 13:32:30 +0100

    chore: redundant script lines deleted

--- assets/scripts/faq.js
+++ assets/scripts/faq.js
@@ -1,17 +1,3 @@
-class FAQ{
-  constructor(faqQuestions,faqAnswers,hideAnswer,revealAnswer){
-    this.theFaqQuestions = faqQuestions
-    this.theFaqAnswers = faqAnswers
-    this.hideAnswerBtn = hideAnswer
-    this.revealAnswerBtn = revealAnswer
-    this.answerRevealed = Boolean
-  }
-
-  // showAnswer(hideAnswerBtn,revealAnswerBtn){
-  //   revealAnswerBtn
-  // }
-}
-
 NodeList.prototype.forEach = NodeList.prototype.forEach || Array.prototype.forEach;
 
 
@@ -22,41 +8,3 @@ const faqAnswers = document.querySelectorAll('[faq-answer]');
 
 const activeFAQ = new FAQ(faqQuestions,faqAnswers)
 
-function toggleButtons(revealAnswer,hideAnswer) {
-  if (activeFAQ.answerRevealed == true) {
-    revealAnswer.style.display = "none"
-
-  }
-}
-
-
-answerRevealed = false;
-
-revealAnswer.forEach(item => {
-  item.addEventListener('click', ()=>{
-  item.setAttribute("id","revealAnswerButton");
-  activeFAQ.answerRevealed = true;
-  console.log(activeFAQ.answerRevealed)
-
-  })
-});
-
-hideAnswer.forEach(button => {
-  button.addEventListener('click', ()=>{
-    button.setAttribute("id","hideAnswerButton");
-    // console.log("clicked")
-    if (answerRevealed = true) {
-      hideAnswerButton.style.display = "block";
-    }
-  })
-});
-
-
-
-function showAnswer(faqQuestions,faqAnswers,hideAnswer,revealAnswer){
-
-    if (this.answerRevealed = true){
-      console.log("answerRevealed")
-    }
-  
-}
