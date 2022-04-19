4677bf1724636cd452942a1ea44d849b949d668c
Qaudri <aquadirmuhammad@gmail.com>
2022-04-19 10:03:00 +0100

    chore: class constructor defined

--- assets/scripts/faq.js
+++ assets/scripts/faq.js
@@ -1,3 +1,12 @@
+class FAQ{
+  constructor(faqQuestions,faqAnswers){
+    this.theFaqQuestions = faqQuestions;
+    this.theFaqAnswers = faqAnswers;
+    this.hideAnswerBtn = hideAnswer;
+    this.revealAnswerBtn = revealAnswer;
+  }
+}
+
 const faqQuestions = document.querySelectorAll('[faq-question]');
 const hideAnswer = document.querySelectorAll('[answer-hide]');
 const revealAnswer = document.querySelectorAll('[answer-reveal]');
@@ -5,8 +14,8 @@ const faqAnswers = document.querySelectorAll('[faq-answer]');
 
 // const activeFAQ = new FAQ(faqQuestions,faqAnswers)
 
-hideAnswer.forEach(image => {
-  image.addEventListener('click', ()=>{
+hideAnswer.forEach(button => {
+  button.addEventListener('click', ()=>{
     hideAnswer.setAttribute('id', 'hideAnswerBtn');
     hideAnswerBtn.style.display = "block"
     console.log("clicked")
