27f18fa631d76a513695a6bfe421cd083351e004
Qaudri <aquadirmuhammad@gmail.com>
2022-04-19 13:23:52 +0100

    implementation in progress

--- assets/scripts/faq.js
+++ assets/scripts/faq.js
@@ -1,14 +1,18 @@
 class FAQ{
-  constructor(faqQuestions,faqAnswers){
+  constructor(faqQuestions,faqAnswers,hideAnswer,revealAnswer){
     this.theFaqQuestions = faqQuestions
     this.theFaqAnswers = faqAnswers
     this.hideAnswerBtn = hideAnswer
     this.revealAnswerBtn = revealAnswer
+    this.answerRevealed = Boolean
   }
+
+  // showAnswer(hideAnswerBtn,revealAnswerBtn){
+  //   revealAnswerBtn
+  // }
 }
 
-// NodeList.prototype.forEach = NodeList.prototype.forEach || Array.prototype.forEach;
-
+NodeList.prototype.forEach = NodeList.prototype.forEach || Array.prototype.forEach;
 
 
 const faqQuestions = document.querySelectorAll('[faq-question]');
@@ -16,23 +20,43 @@ const hideAnswer = document.querySelectorAll('[answer-hide]');
 const revealAnswer = document.querySelectorAll('[answer-reveal]');
 const faqAnswers = document.querySelectorAll('[faq-answer]');
 
-// const activeFAQ = new FAQ(faqQuestions,faqAnswers)
-var answerRevealed = false;
+const activeFAQ = new FAQ(faqQuestions,faqAnswers)
+
+function toggleButtons(revealAnswer,hideAnswer) {
+  if (activeFAQ.answerRevealed == true) {
+    revealAnswer.style.display = "none"
+
+  }
+}
+
+
+answerRevealed = false;
 
 revealAnswer.forEach(item => {
   item.addEventListener('click', ()=>{
   item.setAttribute("id","revealAnswerButton");
-  answerRevealed = true,
-  console.log(answerRevealed);
+  activeFAQ.answerRevealed = true;
+  console.log(activeFAQ.answerRevealed)
+
   })
 });
 
 hideAnswer.forEach(button => {
   button.addEventListener('click', ()=>{
     button.setAttribute("id","hideAnswerButton");
-    answerRevealed = false,
-    console.log("clicked")
+    // console.log("clicked")
+    if (answerRevealed = true) {
+      hideAnswerButton.style.display = "block";
+    }
   })
 });
 
 
+
+function showAnswer(faqQuestions,faqAnswers,hideAnswer,revealAnswer){
+
+    if (this.answerRevealed = true){
+      console.log("answerRevealed")
+    }
+  
+}
