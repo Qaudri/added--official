8207d44f0499e8a5ba6c54a8711e634662f05f75
Qaudri <aquadirmuhammad@gmail.com>
2022-04-20 12:11:02 +0100

    chore: implementation in progress

--- assets/scripts/faq.js
+++ assets/scripts/faq.js
@@ -1,11 +1,3 @@
-NodeList.prototype.forEach = NodeList.prototype.forEach || Array.prototype.forEach;
-
-
-const faqQuestions = document.querySelectorAll('[faq-question]');
-const hideAnswer = document.querySelectorAll('[answer-hide]');
-const revealAnswer = document.querySelectorAll('[answer-reveal]');
-const faqAnswers = document.querySelectorAll('[faq-answer]');
-
 const activeFAQ = new FAQ(faqQuestions,faqAnswers)
 
 
