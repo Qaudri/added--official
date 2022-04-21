4c367e776ad307b3d611c8802ea0d20e6af67533
Qaudri <aquadirmuhammad@gmail.com>
2022-04-21 15:38:09 +0100

    chore: new approach in progress

--- assets/scripts/faq.js
+++ assets/scripts/faq.js
@@ -1,42 +1,15 @@
-var faq = document.getElementsByClassName("faq");
-var answer = document.getElementById("answer");
-var revealBtn = document.getElementById("reveal");
-var hideBtn = document.getElementById("hide");
-var i;
-var r;
-var h;
-var a;
+const items = document.querySelectorAll('.accordion button');
 
-for (i = 0; i < faq.length; i++) {
-    faq = array[i];
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
 }
 
-for (let r = 0; r < revealBtn.length; r++) {
-    revealBtn = array[r];
-}
-
-for (let h = 0; h < revealBtn.length; h++) {
-    hideBtn = array[h];
-}
-
-for (let a = 0; a < revealBtn.length; a++) {
-    revealBtn = array[a];
-}
-
-revealBtn.addEventListener("click", function () {
-    revealBtn.style.display = "none";
-    hideBtn.style.display = "block";
-    answer.classList.add("faq-slide-down");
-    answer.style.display = "block"
-});
-
-
-hideBtn.addEventListener("click", function () {
-    revealBtn.style.display = "block";
-    hideBtn.style.display = "none";
-    answer.classList.replace("faq-slide-down", "faq-slide-up");
-    setTimeout(() => {
-        answer.style.display = "none";
-        answer.classList.remove("faq-slide-up");
-    }, 250);
-});
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
