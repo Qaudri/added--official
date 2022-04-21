c89f8aa64a846dc2e2938525b9bdb86ac93e04ee
Qaudri <aquadirmuhammad@gmail.com>
2022-04-21 14:04:38 +0100

    chore: new function testing in progress

--- assets/scripts/faq.js
+++ assets/scripts/faq.js
@@ -8,11 +8,11 @@ for (i = 0; i < faq.length; i++) {
         this.classList.toggle("active");
 
         /* Toggle between hiding and showing the active panel */
-        var body = this.nextElementSibling;
-        if (body.style.display === "block") {
-            body.style.display = "none";
+        var answer = this.nextElementSibling;
+        if (answer.style.display === "block") {
+            answer.style.display = "none";
         } else {
-            body.style.display = "block";
+            answer.style.display = "block";
         }
     });
 }
