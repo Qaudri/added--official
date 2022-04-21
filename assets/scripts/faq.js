fcbd546d340f1bd0440faefd3bc696fb0d3f87c1
Qaudri <aquadirmuhammad@gmail.com>
2022-04-21 14:49:43 +0100

    chore: faq loop function in progress

--- assets/scripts/faq.js
+++ assets/scripts/faq.js
@@ -1,25 +1,28 @@
 var faq = document.getElementsByClassName("faq");
+var answer = document.getElementById("answer");
+var revealBtn = document.getElementById("reveal");
+var hideBtn = document.getElementById("hide");
 var i;
 
+
 for (i = 0; i < faq.length; i++) {
-    const answer = document.getElementById("answer");
-    const revealBtn = document.getElementById("reveal");
-    const hideBtn = document.getElementById("hide");
-
-    revealBtn.addEventListener("click", function () {
-        revealBtn.style.display = "none";
-        hideBtn.style.display = "block";
-        answer.classList.add("faq-slide-down");
-        answer.style.display = "block"
-    });
-
-    hideBtn.addEventListener("click", function () {
-        revealBtn.style.display = "block";
-        hideBtn.style.display = "none";
-        answer.classList.replace("faq-slide-down", "faq-slide-up");
-        setTimeout(() => {
-            answer.style.display = "none";
-            answer.classList.remove("faq-slide-up");
-        }, 250);
-    })
+    faq = array[i];
 }
+
+revealBtn.addEventListener("click", function () {
+    revealBtn.style.display = "none";
+    hideBtn.style.display = "block";
+    answer.classList.add("faq-slide-down");
+    answer.style.display = "block"
+});
+
+
+hideBtn.addEventListener("click", function () {
+    revealBtn.style.display = "block";
+    hideBtn.style.display = "none";
+    answer.classList.replace("faq-slide-down", "faq-slide-up");
+    setTimeout(() => {
+        answer.style.display = "none";
+        answer.classList.remove("faq-slide-up");
+    }, 250);
+});
